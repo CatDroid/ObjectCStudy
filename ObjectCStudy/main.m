46b5e9b857ad839461311a051a1f3616587238c7
hohanloong <1198432354@qq.com>
2020-10-11 00:11:37 +0800

    Initial Commit

---
+++ ObjectCStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectCStudy
+//
+//  Created by hehanlong on 2020/10/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
