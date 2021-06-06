6dee92935dde2b41083d32b783a3b1ceba174535
hohanloong <1198432354@qq.com>
2021-06-07 01:38:27 +0800

    使用TableView, 实现UITableViewDelegate(响应处理)和UITableViewDataSource(提供显示cell)
    设置UITableView.dataSource和delegate属性

--- App/CoreImageView/CoreImageView/ViewController.m
+++ App/CoreImageView/CoreImageView/ViewController.m
@@ -50,10 +50,26 @@
  */
 
 
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource> // 类扩展 声明要实现的协议
 
-@interface ViewController ()
+@property (nonatomic, strong) UITableView* tableview;
+@property (nonatomic, strong) NSArray* dataSource;
 
 @end
+
+@interface NSIndexPath(ExtendNSIndexPath)
+@end
+
+@implementation NSIndexPath(ExtendNSIndexPath)
+-(void) dumpInfo
+{
+    NSLog(@"NSIndexPath(%p) length=%lu item=%li %li-%li", self, self.length, self.item , self.section, self.row);
+    // length=2 item=0 0-0
+    // length=2 item=3 0-3
+}
+@end
+
+
 
 @implementation ViewController
 {
@@ -62,8 +78,104 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+ 
+    _dataSource = @[
+        @"Filter的使用",
+        @"采用GPU方式实时绘制",
+        @"转场效果",
+        @"人脸识别"
+    ];
+    
+    // self.view.frame 和 self.view.bounds 区别 ??  创建TableView 指定大小 还有样式
+    _tableview = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    _tableview.delegate = self ;    // 需要实现 <UITableViewDelegate> 协议
+    _tableview.dataSource = self;   // 需要实现 <UITableViewDataSource> 协议
+    _tableview.tableFooterView = [UIView new];
+    [self.view addSubview:_tableview]; // 增加子View控件
 }
 
+// UITableViewDataSource 只有两个方法 是 必须要的
+// Only two methods of this protocol are required, and they are shown in the following example code.
+
+// tableview 每一行的 cell
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
+{
+    [indexPath dumpInfo];
+    // dequeueReusableCellWithIdentifier:forIndexPath:
+    UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    cell.textLabel.text = _dataSource[indexPath.row];
+    return cell;
+}
+
+// 返回行的数目
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSLog(@"行的数目 %lu", _dataSource.count);
+    return _dataSource.count;
+}
+
+/*  UITableViewDelegate协议 还有很多
+- (void)encodeWithCoder:(nonnull NSCoder *)coder {
+    <#code#>
+}
+
+- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
+    <#code#>
+}
+
+- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    <#code#>
+}
+
+- (CGSize)sizeForChildContentContainer:(nonnull id<UIContentContainer>)container withParentContainerSize:(CGSize)parentSize {
+    <#code#>
+}
+
+- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    <#code#>
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    <#code#>
+}
+
+- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    <#code#>
+}
+
+- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
+    <#code#>
+}
+
+- (void)setNeedsFocusUpdate {
+    <#code#>
+}
+
+- (BOOL)shouldUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context {
+    <#code#>
+}
+
+- (void)updateFocusIfNeeded {
+    <#code#>
+}
+*/
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    switch(indexPath.row)
+    {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            NSLog(@"unknown selection %li", indexPath.row);
+            break;
+    }
+}
 
 @end
