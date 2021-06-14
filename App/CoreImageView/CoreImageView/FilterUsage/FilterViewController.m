9b6b389a26bbb9492ab85698b16a2900d72b85ef
CatDroid <1198432354@qq.com>
2021-06-14 22:55:00 +0800

    使用CFAllocator来观察CFString的释放时机; 当CFString __bridge NSString之后, 只CFRelease了CFString, 并不会导致内存释放,知道NSString释放

--- App/CoreImageView/CoreImageView/FilterUsage/FilterViewController.m
+++ App/CoreImageView/CoreImageView/FilterUsage/FilterViewController.m
@@ -11,6 +11,7 @@
 {
     UIImageView* effectedImageView ;
     NSInteger filer1Index ;
+    UIButton* filterButton0;
     UIButton* filterButton1;
     UIButton* filterButton2;
 }
@@ -25,7 +26,7 @@
     
     self.view.backgroundColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0];
     [self setupUI];
-    
+  
     NSLog(@"FilterView viewDidLoad ");
     
 }
@@ -55,6 +56,18 @@
     
     [self.view addSubview:effectedImageView];
     
+    
+    filterButton0 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
+    filterButton0.center = CGPointMake(50, self.view.frame.size.height * 5 / 8);
+    [filterButton0 setTitle:@"bridge测试" forState:UIControlStateNormal]; // 未按下
+    [filterButton0 setTitle:@"按下bridge" forState:UIControlStateHighlighted]; // 按下
+    [filterButton0 setBackgroundColor:[UIColor orangeColor]];
+    [filterButton0 setAlpha:0.6];
+    [filterButton0 addTarget:self action:@selector(bridgeStringTest) forControlEvents:UIControlEventTouchDown];
+    // setBackgroundImage setTitle 都有 按下 聚集 释放 平时 等各种状态下 应该显示的文字和图片切换
+    [self.view addSubview:filterButton0];
+    
+    
     filterButton1 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
     filterButton1.center = CGPointMake(50, self.view.frame.size.height * 6 / 8);
     [filterButton1 setTitle:@"系统滤镜" forState:UIControlStateNormal]; // 未按下
@@ -76,6 +89,142 @@
     
     [self.view addSubview:filterButton2];
     
+}
+
+
+#pragma mark - CFAllocator
+
+void *customAlloc(CFIndex size, CFOptionFlags hint, void *info)
+{
+    void* ptr = malloc(size);
+    NSLog(@"分配内存 %p size = %ld", ptr, size);
+    return ptr;
+}
+
+void *customRealloc(void *ptr, CFIndex newsize, CFOptionFlags hint, void *info)
+{
+    NSLog(@"警告:发生了内存重新分配");
+    return NULL;//不写这个回调系统也是返回NULL的。这里简单的打句log。
+}
+
+void customDealloc(void *ptr, void *info)
+{
+    NSLog(@"释放内存 %p", ptr);
+    free(ptr);
+}
+
+CFAllocatorRef customAllocator(void* contextInfo)
+{
+    CFAllocatorRef allocator = NULL;
+    if (NULL == allocator)
+    {
+        /*
+         typedef struct {
+             CFIndex                                version;
+             void *                                 info;
+             CFAllocatorRetainCallBack              retain;
+             CFAllocatorReleaseCallBack             release;
+             CFAllocatorCopyDescriptionCallBack     copyDescription;
+             CFAllocatorAllocateCallBack            allocate;
+             CFAllocatorReallocateCallBack          reallocate;
+             CFAllocatorDeallocateCallBack          deallocate;
+             CFAllocatorPreferredSizeCallBack       preferredSize;
+         } CFAllocatorContext;
+         
+         */
+        CFAllocatorContext context = {0, contextInfo, NULL, NULL, NULL, customAlloc, customRealloc, customDealloc, NULL};
+        allocator = CFAllocatorCreate(kCFAllocatorSystemDefault, &context);
+    }
+    return allocator ;
+}
+
+void releaseCustomAllocator(CFAllocatorRef allocator)
+{
+    CFAllocatorDeallocate(kCFAllocatorSystemDefault, (void*)allocator); // 不需要&取地址
+}
+
+-(void) bridgeStringTest
+{
+    CFAllocatorRef allocator = customAllocator(NULL);
+    [self __bridgeStringTest: allocator];
+    NSLog(@"return from __bridgeStringTest");
+    // releaseCustomAllocator(allocator); // [testNS UTF8String]; 加上这里 release了 就不会出现 CFString释放??
+}
+
+
+-(void) __bridgeStringTest:(CFAllocatorRef) allocator
+{
+    
+    //------------------------------------------------------------------------------------------------------------
+    // char* <------> NSString
+    //
+    //
+    const char *cString2 = "I am char";
+    NSString* aNSString1 = [NSString stringWithUTF8String:cString2];
+    NSLog(@"char* ------> CFStringRef:%@", aNSString1);
+    
+
+    //------------------------------------------------------------------------------------------------------------
+    // NSString <------> CFString
+    //
+    //
+    NSString *aNSString = @"NSString"; // typedef struct __CFString CFStringRef;
+    CFStringRef aCFString = (__bridge CFStringRef)aNSString; // NSString -> CFStringRef
+    NSString *bNSString = (__bridge NSString *)aCFString;    // CFStringRef -> NSString
+    NSLog(@"NSString %p", aNSString);
+    NSLog(@"NSString ------> CFStringRef : %@ ", aCFString );
+    NSLog(@"CFStringRef ------> NSString : %@ %p class is %@", bNSString, bNSString, [bNSString class]); // __NSCFConstantString bNSString就是aNSString
+
+    //------------------------------------------------------------------------------------------------------------
+    // char* ------> CFStringRef
+    //
+    //
+   
+    char* cStringA = malloc(256); // char *cStringA = "测试字符串"; // 实际是const char*
+    snprintf(cStringA, 256, "字符串-%s","测试字符串");
+    NSLog(@"%s length is %lu", cStringA, strlen(cStringA)); // =  25 allocator分配给NSString 64
+    
+    //CFStringRef bCFString = CFStringCreateWithCString(NULL, cStringA, kCFStringEncodingUTF8);
+    CFStringRef bCFString = CFStringCreateWithCString(allocator, cStringA, kCFStringEncodingUTF8); // 使用自己的Allocator来分析内存的释放
+    free(cStringA);
+    NSLog(@"char* ------> CFStringRef:%@", bCFString);
+    
+    // CFRelease(bCFString); // 放在这里 下面一句会 EXC_BAD_ACCESS 地址不是0
+   
+   
+    //------------------------------------------------------------------------------------------------------------
+    // CFStringRef ------> NSString
+    //
+    //
+    NSString *testNS = (__bridge NSString *)bCFString;
+    // CFRetain(CFTypeRef cf)
+    
+    // CFRelease和CGImageRelease的区别在于
+    // CFRelease的参数不能为NULL，
+    // CGImageRelease可以接受NULL参数
+    
+    CFRelease(bCFString); // 因为上面有NSString 所以到这里都不会释放 CFStringCreateWithCString 但如果没有了NSString 就会在这里返回前释放
+    NSLog(@"调用 CFRelease 之后, __bridge的NSString %@ class:%@", testNS, [testNS class]); // __NSCFConstantString
+    
+    //------------------------------------------------------------------------------------------------------------
+    // CFStringRef ------> char*
+    //
+    //
+    const char *testC = [testNS UTF8String]; // !!! 这个返回的是NSString内部的 所以指针不用释放 但是要用到其他地方就要copy
+    char *testCNew = (char *)malloc(strlen(testC) + 1); //  [testNS UTF8String]; 会导致 释放时间更加晚 在调用函数再返回之后才释放
+    memcpy(testCNew, testC, strlen(testC) + 1);
+    NSLog(@"CFStringRef ------> char* :%s",testCNew);
+    free(testCNew);
+    
+
+    //------------------------------------------------------------------------------------------------------------
+    CFStringRef justCFString = CFStringCreateWithCString(allocator, "hello world", kCFStringEncodingUTF8);
+    NSLog(@"CFStringRef 没有__bridge转化成NSString %@", justCFString);
+    CFRelease(justCFString);
+    NSLog(@"因此, 上面的调用返回之前 就会释放了(观察allocator的dealloc打印)");
+    
+    // 要等所有使用的地方释放了，才能释放！
+    // releaseCustomAllocator(allocator);
 }
 
 #pragma mark - Filer
@@ -162,6 +311,15 @@
 - (IBAction) filter2Button:(UIButton*)sender forEvent:(UIEvent*)event
 {
     NSLog(@"use filter 2");
+    
+    
+    
+    // 从Cocoa Foundation转Core Foundation，ARC管理内存，不需要释放：
+    // 从Core Foundation转Cocoa Foundation，需要开发者手动释放，不归ARC管：
+    
+    //
+    
+    
     
     
 }
